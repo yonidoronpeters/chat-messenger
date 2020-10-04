fdf1e30bf07b7e177c7d1bc5dad406257f45e8d3
Yoni Doron-Peters <jdoronpeters@gloo.us>
2020-10-04 10:29:30 -0600

    more cleanup

--- src/message/messageController.ts
+++ src/message/messageController.ts
@@ -1,11 +1,4 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Post,
-  Render,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { MessageService } from './message.service';
 
 @Controller()
@@ -13,8 +6,8 @@ export class MessageController {
   constructor(private readonly appService: MessageService) {}
 
   @Get()
-  root(): { pong: string} {
-    return { pong: 'hello world' };
+  root(): string {
+    return 'Please visit https://cryptochatter-client.herokuapp.com to start chatting';
   }
 
   @Post('form')
@@ -27,16 +20,5 @@ export class MessageController {
   @Get('messages')
   async getMessages(): Promise<MsgDto[]> {
     return this.appService.getMessages();
-  }
-
-  @Post('/login-user')
-  async loginUser(@Body() dto: UserDto): Promise<UserDto> {
-    console.log('validating username');
-    if (dto.name?.trim()) {
-      console.log(`${dto.name} is valid`);
-      return dto;
-    }
-    console.log(`${dto.name} is invalid`);
-    throw new BadRequestException(`${dto.name} is invalid`);
   }
 }
